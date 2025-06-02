f842af09619794e18151f41d9fd2782cba255f14
Samiakiya <591638@edu.rocmn.nl>
2025-06-02 14:41:40 +0200

    portfolio veranderd en updated

---
+++ home.js
@@ -0,0 +1,23 @@
+function scrollToSection(id) {
+    const el = document.getElementById(id);
+    if(el) {
+      el.focus();
+      el.scrollIntoView({behavior: 'smooth'});
+    }
+  }
+
+
+  function submitForm(event) {
+    event.preventDefault();
+    const name = event.target.name.value.trim();
+    const email = event.target.email.value.trim();
+    const message = event.target.message.value.trim();
+
+
+    if(name && email && message) {
+      alert(`Bedankt, ${name}! Je bericht is succesvol verzonden.`);
+      event.target.reset();
+    } else {
+      alert('Vul alstublieft alle velden correct in.');
+    }
+  }
